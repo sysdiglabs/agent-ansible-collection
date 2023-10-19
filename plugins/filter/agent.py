8fc8aa127c7eaed2ff9963605b0e29b59afa4c25
Michael Przybylski <michael.p@sysdig.com@sysdig.com>
2023-10-19 08:58:58 -0700

    Add support for Universal eBPF
    Add Universal eBPF config generation plumbing to install role.
    
    Add molecule tests for Universal eBPF installation.
    
    Update driver naming to be consistent with kmod / universal_ebpf / legacy_ebpf
    naming convention used elsewhere

--- plugins/filter/agent.py
+++ plugins/filter/agent.py
@@ -1,9 +1,15 @@
 def to_agent_driver_type(data):
     """ Return the desired Sysdig Agent driver type """
     try:
-        return data['agent']['driver']['type']
+        user_input = data['agent']['driver']['type']
+        if user_input == "ebpf":
+            return "legacy_ebpf"
+        if user_input == "kmodule":
+            return "kmod"
+        else:
+            return user_input
     except KeyError:
-        return "kmodule"
+        return "kmod"
 
 
 def to_agent_version(data):
@@ -16,7 +22,7 @@ def to_agent_version(data):
 
 
 def to_agent_install_probe_build_dependencies(data):
-    """ Return true or false depending on if the probe (ebpf|kmodule) build
+    """ Return true or false depending on if the probe (legacy_ebpf|kmod) build
     dependencies should be installed
     """
     try:
