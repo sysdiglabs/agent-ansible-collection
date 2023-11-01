bfc01fa0da4ad0bc30f5ff1c82931b270369c977
Mike Przybylski <mikep@gimmethebrain.net>
2023-11-01 10:22:58 -0700

    feat: Add support for Universal eBPF [SSPROD-26836] (#211)
    Co-authored-by: Michael Przybylski <michael.p@sysdig.com@sysdig.com>
    Co-authored-by: Marco Vito Moscaritolo <mavimo@gmail.com>

--- plugins/filter/agent.py
+++ plugins/filter/agent.py
@@ -1,9 +1,18 @@
+from ansible.utils.display import Display
 def to_agent_driver_type(data):
     """ Return the desired Sysdig Agent driver type """
     try:
-        return data['agent']['driver']['type']
+        user_input = data['agent']['driver']['type']
+        if user_input == "ebpf":
+            Display().warning("'ebpf' is a deprecated option for agent.driver.type.  Please use 'legacy_ebpf' instead.")
+            return "legacy_ebpf"
+        if user_input == "kmodule":
+            Display().warning("'kmodule' is a deprecated option for agent.driver.type.  Please use 'kmod' instead.")
+            return "kmod"
+        else:
+            return user_input
     except KeyError:
-        return "kmodule"
+        return "kmod"
 
 
 def to_agent_version(data):
@@ -16,7 +25,7 @@ def to_agent_version(data):
 
 
 def to_agent_install_probe_build_dependencies(data):
-    """ Return true or false depending on if the probe (ebpf|kmodule) build
+    """ Return true or false depending on if the probe (legacy_ebpf|kmod) build
     dependencies should be installed
     """
     try:
